3fd2266d6659eb3237b8084f5af47fd72cd1efe6
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-03-13 19:16:54 +0600

    react props is added in github

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,11 +1,29 @@
 import "./App.css";
 import Component from "./component";
+import Props3 from "./props";
+import Props2 from "./props";
+import Props from "./props";
 
 function App() {
+  // const person = {
+  //   name: "Noyon",
+  //   email: "noyon-1@gmail.com",
+  //   phone: "01585735520",
+  // };
+
+  // const person2 = {
+  //   name: "Noyon",
+  //   email: "noyon-1@gmail.com",
+  //   phone: "01585735520",
+  // };
+
   return (
     <>
       <h1>Vite + React</h1>
-      <Component></Component>
+      {/* <Component></Component> */}
+      {/* <Props name={"Noyon"} email={"noyon-1@gmail.com"} phone={"01585735520"}></Props> */}
+      {/* <Props2 person={person}></Props2> */}
+      {/* <Props3 person={person2}></Props3> */}
     </>
   );
 }
