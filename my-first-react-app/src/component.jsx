12a590c3d9b7f62b6edb2f97e335245fda440415
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-03-13 18:55:31 +0600

    react component is added in github

---
+++ my-first-react-app/src/component.jsx
@@ -0,0 +1,9 @@
+export default function Component() {
+    return(
+        <div>
+            <h1>This My First React Component</h1>
+            <h2>This is the beginning this is the very very beginning in my React Journey</h2>
+            <button>Click Me</button>
+        </div>
+    )
+}
