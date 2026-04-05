f285fefa7cc9bf7fcc3d546f05abf04fa6111dd7
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-04-05 21:58:50 +0600

    ...

--- my-first-react-app/src/UseContext.jsx
+++ my-first-react-app/src/UseContext.jsx
@@ -1,43 +1,43 @@
-import { createContext, useContext } from 'react';
+// import { createContext, useContext } from 'react';
 
-const ThemeContext = createContext(null);
+// const ThemeContext = createContext(null);
 
-export default function MyApp() {
-  return (
-    <ThemeContext value="dark">
-      <Form />
-    </ThemeContext>
-  )
-}
+// export default function MyApp() {
+//   return (
+//     <ThemeContext value="dark">
+//       <Form />
+//     </ThemeContext>
+//   )
+// }
 
-function Form() {
-  return (
-    <Panel title="Welcome">
-      <Button>Sign up</Button>
-      <Button>Log in</Button>
-    </Panel>
-  );
-}
+// function Form() {
+//   return (
+//     <Panel title="Welcome">
+//       <Button>Sign up</Button>
+//       <Button>Log in</Button>
+//     </Panel>
+//   );
+// }
 
-function Panel({ title, children }) {
-  const theme = useContext(ThemeContext);
-  const className = 'panel-' + theme;
-  return (
-    <section className={className}>
-      <h1>{title}</h1>
-      {children}
-    </section>
-  )
-}
+// function Panel({ title, children }) {
+//   const theme = useContext(ThemeContext);
+//   const className = 'panel-' + theme;
+//   return (
+//     <section className={className}>
+//       <h1>{title}</h1>
+//       {children}
+//     </section>
+//   )
+// }
 
-function Button({ children }) {
-  const theme = useContext(ThemeContext);
-  const className = 'button-' + theme;
-  return (
-    <button className={className}>
-      {children}
-    </button>
-  );
-}
+// function Button({ children }) {
+//   const theme = useContext(ThemeContext);
+//   const className = 'button-' + theme;
+//   return (
+//     <button className={className}>
+//       {children}
+//     </button>
+//   );
+// }
 
 
