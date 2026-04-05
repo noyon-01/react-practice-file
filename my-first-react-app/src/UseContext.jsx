b64645b6aaee8c748c03d1014ad67a6b555bc4da
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-04-05 21:57:07 +0600

    usecontext is added

--- my-first-react-app/src/UseContext.jsx
+++ my-first-react-app/src/UseContext.jsx
@@ -145,3 +145,55 @@ function Button({ children, onClick }) {
   );
 }
 
+import { createContext, useContext } from 'react';
+
+// const ThemeContext = createContext(null);
+
+export default function MyApp() {
+  return (
+    <ThemeContext value="dark">
+      <Form />
+    </ThemeContext>
+  )
+}
+
+function Form() {
+  return (
+    <Panel title="Welcome">
+      <Button>Sign up</Button>
+      <Button>Log in</Button>
+      <ThemeContext value="light">
+        <Footer />
+      </ThemeContext>
+    </Panel>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <Button>Settings</Button>
+    </footer>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'panel-' + theme;
+  return (
+    <section className={className}>
+      {title && <h1>{title}</h1>}
+      {children}
+    </section>
+  )
+}
+
+function Button({ children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'button-' + theme;
+  return (
+    <button className={className}>
+      {children}
+    </button>
+  );
+}
